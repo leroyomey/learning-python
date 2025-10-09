a42b20e11e48cb3cf4062884cdaa9ce455c10c03
omey <leroyomey@gmail.com>
2025-10-09 12:37:12 -0600

    Add files via upload

--- pwGenerator.py
+++ pwGenerator.py
@@ -2,6 +2,7 @@ import random
 import string
 import secrets
 import sys
+from pathlib import Path
 
 class Colors:
     BLACK = "\033[0;30m"
@@ -15,40 +16,58 @@ class Colors:
     RESET = "\033[0m"
 
 randomChar = string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
-
+   
 def generateRandomPassword():
     passwordLength = random.randint(8, 24)
     passwordRandom = ''.join(secrets.choice(randomChar) for _ in range(passwordLength))
-    print(passwordRandom)
+    print(f"{Colors.MAGENTA}{passwordRandom}{Colors.RESET}")
+    return passwordRandom
 
-def continueOrExit():
-    userInput = input(f"{Colors.YELLOW}Continue? (y/n): {Colors.RESET}")
+def saveContinueExit():
+    result = generateRandomPassword()
+    
+    print(f"{Colors.GREEN}[1] Continue{Colors.RESET}")
+    print(f"{Colors.CYAN}[2] Save Password{Colors.RESET}")
+    print(f"{Colors.RED}[3] Exit{Colors.RESET}")
 
-    inputA = "y", "Y"
-    inputB = "n", "N"
+    userInput = int(input(f"{Colors.YELLOW}Enter option (1/2/3): {Colors.RESET}"))
 
-    if userInput in inputA:
-        generateRandomPassword()
-        continueOrExit()
-    elif userInput in inputB:
+    inputA = 1
+    inputB = 2
+    inputC = 3
+
+    if userInput == inputA:
+        saveContinueExit()
+
+    elif userInput == inputB:
+        fileDirectory = Path.cwd() 
+        with open("password.txt", "w") as file:
+            file.write(result)
+        print(f"{Colors.GREEN}Saved{Colors.RESET} {result} in {Colors.GREEN}{fileDirectory}{Colors.RESET}")
+        saveContinueExit()
+
+    elif userInput == inputC:
         sys.exit()
-    else: 
-        print(f"{Colors.RED}Error!{Colors.RESET}")
-        continueOrExit()
 
-def generateMethod():
+    else: 
+        userInput != inputA or inputB or inputC
+        print("Error!")
+        saveContinueExit()
+
+def mainMenu():
     print(f"{Colors.CYAN}Password Generator{Colors.RESET}")
     print(f"{Colors.GREEN}[1] Generate Random Password (8-24){Colors.RESET}")
     print(f"{Colors.RED}[2] Exit{Colors.RESET}")
 
     userInput = int(input(f"{Colors.YELLOW}Enter option (1/2): {Colors.RESET}"))
 
-    if userInput == 1:
-        generateRandomPassword()
-        continueOrExit()
-    if userInput == 2:
+    inputA = 1
+    inputB = 2
+
+    if userInput == inputA:
+        saveContinueExit()
+
+    if userInput == inputB:
         sys.exit()
-    else: pass
 
-
-generateMethod()
+mainMenu()
